17a87dfc514baf4da87f58c37fda69994408a594
Oleg <saga6021@gmail.com>
2020-05-20 17:04:37 +0700

    Some changes #3

--- flashapp/some_app.py
+++ flashapp/some_app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return " <html><head></head> <body> <i><h1>Hello World!</h1></i> </body></html>"
+
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000)
+    app.run(host='127.0.0.1', port=5000)
